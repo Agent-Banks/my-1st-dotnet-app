0ca29f3e43ca39a521289cbd22e6cdb14f00a296
CodyBanks <codydouglasbanks@gmail.com>
2020-05-12 13:08:33 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace my_1st_dotnet_app
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to C#");
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# my_1st_dotnet_app
